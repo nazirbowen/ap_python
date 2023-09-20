c095dfc3c6fb5fd8b987a8fd9a18f4e8ccc04a66
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-09-20 15:25:02 +0000

    classwork sept 20

--- Appythonactivites/builtinFunctions.py
+++ Appythonactivites/builtinFunctions.py
@@ -3,3 +3,4 @@ print(type(is_it_a_week_day = True)
 numberofbooks = 10
 print(type(numberofbook))
 
+   
---
+++ Appythonactivites/dataCastingActivities.py
@@ -0,0 +1,21 @@
+# convert the following datatypes into floats.
+
+a = float(12)
+b = float(100)
+print(a)
+print(b)
+
+c = int(12.34)
+d = int(23.33)
+print(c)
+print(d)
+
+num_one = int(20)
+num_two= int(23.45)
+print(num_one)
+print(num_two)
+
+name_of_pet = "Spuny"
+age_of_pet = 10
+
+print("this is my dog "+ name_of_pet + ". He is " + age_of_pet+ " years old.")
