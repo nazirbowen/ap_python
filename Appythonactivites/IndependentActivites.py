ae168ef17b42885aeb976586e9a567ef93ed1e98
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-10-06 15:23:15 +0000

    finish

--- Appythonactivites/IndependentActivites.py
+++ Appythonactivites/IndependentActivites.py
@@ -13,10 +13,13 @@ linkTwo= 'https://afternerd.com/blog/python-lists-for-absolute-beginners/'
 
 # 1.Create a simple list variable that contains 5 list items. It is up to you what will be in your list and what 
 # data type they will be. 
-
-mylist = ["apple", "banana", "cherry", "doughnut","eggs"]
-
 # Some examples: favorite_atheletes, favorite_games, favorite_books, etc.  
+
+myFoodList=["pizza ","burger ","cake ", "pop tarts"]
+print(myFoodList)
+
+# what is a list?
+# a list is a way to store multiple values in a variable. 
 
 # 2. Find and print the specific item in each list based on their index in the list
 # HINT you will need to use a python built-in function that specifically lets you find items in a Python list. 
@@ -24,18 +27,30 @@ mylist = ["apple", "banana", "cherry", "doughnut","eggs"]
 # find and print index 3
 zoo_animals = ['wolf','giraffe','hippo','eagle','parrot']
 
+za=zoo_animals.index('eagle')
+print(za)
+
+print(zoo_animals[3])
+
 # find and print index 1
 sports_on_tv =['hockey','football','baseball','soccer','racing']
+
+
 
 # find and print index 0
 random_numbers = [10,100,12123, 1394, 1]
 
-
 # 3. Create a program that will only print out the odd numbers in this list. 
-
 # HINT- part of solving this is that you will need to use the range() function. 
 
+# we know we need to find and print the odd numbers.
+# range() functions 
 number_list= [1,2,3,4,5,6,7,8,9,10]
+r= range(0,10)
+
+odd= [x for x in range(10) if x%2==0]
+print(odd)
+
 
 # 4. You have been hired by amazon to be an engineer. Your first assignment is to fix their
 # shopping cart function. Your goal is to create a line of code that will
@@ -46,3 +61,47 @@ number_list= [1,2,3,4,5,6,7,8,9,10]
 # HINT - for this function you will need to use the append() function. 
 
 shopping_cart = ['notebook', 'pens','tape','mousepad']
+
+def updatingShoppingCart():
+    add_item=input('please add and item to your cart')
+    shopping_cart.append(add_item)
+    print(shopping_cart)
+
+# updatingShoppingCart()
+  
+
+# Python List - a data type that allows for storing multiple values in a vairable. 
+# a Python lists uses [] square brackets for the values. 
+number_list= [101,10239, 10394, 10394]
+string_list=['kevin']
+var_list=[number_list, string_list]
+
+print(var_list)
+
+apples=[2.00,'apple description']
+tvDinner=[4.00,'sphagetti and meatballs']
+water=[3.00,'20 onces']
+
+self_checkout_scanner=[apples, tvDinner, water]
+print(len(self_checkout_scanner))
+
+# type of container
+# objects, strings, other data types inside of list
+
+
+# create a function that will add a new list item to a checkout cart
+# the user should be able to enter the name of the item and the price.
+# your function should add the name of the item to the list of items
+# you will also need to write code that will add all the prices of the items
+# including the price of the new item.
+
+# keywords:
+
+#clues: append() function, we need strings, 
+
+# starter code
+list__of_items=['apple', 'organe','book']
+
+apple_price= 1.00
+orange_price=3.00
+book_price=10.00
---
+++ Appythonactivites/Quiz2.py
@@ -0,0 +1,92 @@
+# 1. Create a function that will multiply two (2) values that are passed into the function as arguments. 
+# Your function should print out the result of the two numbers that have been multiplied.
+num1 = 12
+num2 = 12
+print(12 * 12)
+num3= 144
+
+# 2. Create a function that will do the following calculation. Your function should take in three argument. it should add the first
+# two arguments and then the sum of the first two (2) should be divided by the third argument. 
+# You function should then print the result. 
+
+print(num1 + num2 //num3)
+
+
+# 3. Create a elevator function that will run specific lines of code based on the conditions provided by a user. If the user types in 101,
+# the function should print out they are going to the boys latin office, if they type in 203, they are going to the gym, 
+# and if they type in the letter g, the lobby. else, if the input doesnt match any of the values provided, tell the user that floor doesn't exist and to please
+# enter a valid floor number.
+
+def ToOffice():
+   if  ("user types 101")
+        print("going to boyslatin office")
+   
+   if  ("user types 203")
+        print('going to the gym')    
+   
+    if  (" user types g)
+         print(going to lobby)
+   
+    else: ("floor doesn't exist")
+        print("please enter a valid floor number")
+def ToOffice():
+
+# 4. Write a simple conditional statement that uses a boolean that will print if it is daytime or nighttime.
+
+def TimeofDay ():
+if  (" time is 800am")
+        print("it is day")
+ 
+else: (" time is 800pm")
+  print(" it is night")
+def TimeofDay ():
+
+
+# 5. What function would you use if you wanted to add and element/ value to a list data type? Explain why you would use it.
+You would use the append() function to put the new item in an spot
+
+# 6. Do some research and find the correct built-in python function that will reverse the order of the following list.
+# then print your list in the reverse order.
+You can use the reversed() function
+
+RNL = [0,1,2,3,4,5,6,7,8]
+ralph = reversed(RNL)
+for x in ralph:
+  print(x)
+
+# 7.Do some research and find the correcrt built-in python function that will allow you to find the largest number in the following list.
+# then print the largest number
+
+ranom_number_list2 = [100,230,40,39403,19]
+RNL2 = [100,230,40,39403,19]
+x = sorted(RNL2)
+print(x)
+
+# 8. A security company has hired you as an engineer to help them develop a program that will only let users into the building 
+# if they enter a specific password. They given you the following information to use to build this program.
+# - they want users to be able to enter a series of codes to get access
+# - they want the user to enter an initial password value which is 0001
+# - if they get this correct, they then want them to enter another value, which is 3039
+# - if this is correct they will get access to the building
+# - if they have the wrong answer in either scenario they will get a message saying access denied. 
+
+def SecurityPassword():
+   if  ("user types 0001")
+        print("Password Correct , procced")
+   
+   if  ("user types 3039")
+        print('Access granted! Welcome')    
+   
+     elif: (" user enters wrong password")
+        print(" Access denied")
+def SecurityPassword():
+
+# 9. What does it mean to call a function? Why do we call functions. 
+# you can use the variable below to enter you ansewer. 
+answer9=' We usally call our function so that we dont have to rewrite the code. We also give it a name to call by later ' 
+
+
+# 10. Find and print each value at the specific indexes provided in the list.
+# find and print the values/words at index 3, 4, and 6 
+
+shopping_cart = ['apples','water','chicken','ice cream','ground beef','string beans','oranges']
