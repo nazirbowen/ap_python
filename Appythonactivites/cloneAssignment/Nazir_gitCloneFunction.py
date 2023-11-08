aac897050d9e38ff98233be2187fbf8a8ceed40e
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-11-08 01:35:25 +0000

    done

--- Appythonactivites/cloneAssignment/Nazir_gitCloneFunction.py
+++ Appythonactivites/cloneAssignment/Nazir_gitCloneFunction.py
@@ -1,9 +1,8 @@
 def profileData():
-    Goal = input('what is your ideal weight goal?')
-    name = input('whats your name?')
-    picture = input('upload a before picture')
-    CurrentWeight= int(input('What is your current weight'))
-    WeightCounter = float(input('How much would you like to lose or gain'))
+    name = input('whats your name ? ')
+    Goal = float(input('what is your ideal weight goal ? '))
+    CurrentWeight= int(input('What is your current weight ? '))
+    WeightCounter = int(input('How much would you like to lose or gain ? '))
     print('Welcome to your weight journey.')
-
+    
 profileData()
