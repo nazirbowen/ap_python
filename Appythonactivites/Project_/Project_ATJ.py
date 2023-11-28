c8aaef8b1bc906526624b6cbb417c1a8dc6ccaf3
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-11-28 16:16:27 +0000

    somewhat done

---
+++ Appythonactivites/Project_/Project_ATJ.py
@@ -0,0 +1,11 @@
+def profileData():
+    
+    name = input('whats your name ? ')
+    Goal = float(input('what is your ideal income ? '))
+    CurrentProfession = (input('What are things youre good at ? '))
+    Location = (input('Where are you located ? '))
+    PreviousApplied = (input('What are some places you have applied ? '))
+    PreviouslyWorked = (input(' What are some places you have worked ? '))
+    print('Welcome to Auto Job finder.')
+    
+profileData()
