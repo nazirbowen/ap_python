3aeb679ea29d6576cdbecbe8e77d0f28f57d499c
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-11-29 21:53:06 +0000

    done

--- Appythonactivites/Project_/Project_ATJ.py
+++ Appythonactivites/Project_/Project_ATJ.py
@@ -1,11 +1,20 @@
+# My program is designed to help users in need of jobs. 
+# The program is going to take in information and redirect people to jobs in need in their area.
+
+
 def profileData():
-    
     name = input('whats your name ? ')
+    Age = float(input('what is your age ? '))
     Goal = float(input('what is your ideal income ? '))
     CurrentProfession = (input('What are things youre good at ? '))
     Location = (input('Where are you located ? '))
     PreviousApplied = (input('What are some places you have applied ? '))
     PreviouslyWorked = (input(' What are some places you have worked ? '))
+    
+    print('Here are some jobs hiring in your area... ')
+
+    
+    
     print('Welcome to Auto Job finder.')
     
-profileData()
+profileData() 
